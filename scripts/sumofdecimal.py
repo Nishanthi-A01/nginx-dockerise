bb36df8babef4eac57b5ae416e6a13652c0844d0
Nishanthi-A01 <79563628+Nishanthi-A01@users.noreply.github.com>
2024-11-30 19:57:57 +0530

    sum of decimal number

--- scripts/sumofdecimal.py
+++ scripts/sumofdecimal.py
@@ -1,15 +1,15 @@
-from decimal import *
+from decimal import Decimal
 def sum_n(x, n):
     s = 0
     for i in range(1,n+1):
-        s += decimal(('%d'*i) % tuple([x]*i))
+        s += Decimal(('%d'*i) % tuple([x]*i))
     return s
 
 
 if __name__ == "__main__":
    decimalnum = input("Enter the decimal number:")
    iterations = input("Enter the number of iterations:")
-   total=sum_n(decimal(decimalnum),int(iterations))
+   total=sum_n(Decimal(decimalnum),int(iterations))
    print(total)
 
 
