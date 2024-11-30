3892303c99fd1b30c0391dba5b9664132cbbf173
Nishanthi-A01 <79563628+Nishanthi-A01@users.noreply.github.com>
2024-11-30 11:07:16 +0530

    adding sum of even fibonacci number

--- scripts/sumfibonacci_2.py
+++ scripts/sumfibonacci_2.py
@@ -1,14 +1,24 @@
 #This script calculates the even fibonacci number out of first 100 fibonacci sequence 
 
-n = 100 # determine the number of finonacci sequence 
-num1 = 0  # declaring the first two fibonacci number 
-num2 = 1
-next_number = num2  
-count = 1
+def fibonacciseq(target)
 
-while count <= n:
-    print(next_number, end=" ")
-    count += 1
-    num1, num2 = num2, next_number
-    next_number = num1 + num2
-print()
+    # determine the number of finonacci sequence 
+    num1 = 0  # declaring the first two fibonacci number 
+    num2 = 1
+    next_number = num2  
+    count = 1
+
+    while count <= n:
+        print(next_number, end=" ")
+        count += 1
+        num1, num2 = num2, next_number
+        next_number = num1 + num2
+        print(" the list of first ${target} fibonacci dequence:")
+        print()
+return next_number
+
+print(" the list of first ${target} fibonacci dequence:")
+
+target = 100 
+
+sum([i for i in fibonacci(target) if i%2 ==0])
