5d01978ea20ec20277cab7697304b1c4f3d79cb3
Nishanthi-A01 <79563628+Nishanthi-A01@users.noreply.github.com>
2024-12-01 00:37:25 +0530

    adding shell scripting

--- scripts/shellscripts.sh
+++ scripts/shellscripts.sh
@@ -4,14 +4,14 @@
 echo Testing the grep command:
 #echo &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-echo #####greping nginx-deployment in input.txt file ############
+echo greping nginx-deployment in input.txt file 
 grep "nginx-deployment" ./input.txt
 
-echo #####greping 4 lines before the search item volume  in input.txt file ############
+echo greping 4 lines before the search item volume  in input.txt file
 grep -B 4 "volume" ./input.txt
-echo #####greping file with name sum and .py extension  ############
+echo greping file with name sum and .py extension  
 grep -l -i sum *.py
-echo #####greping file without name sum and .py extension  ############
+echo greping file without name sum and .py extension 
 grep -L -i sum *.py
 
 #echo &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
@@ -19,16 +19,16 @@ echo Testing sed command with the input file:
 #echo &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
 
-echo #####replacing docker with dockerhub using sed ############
+echo replacing docker with dockerhub using sed 
 sed -e' s/docker /dockerhub/g' input.txt > inputmodified.txt
 
-echo #####Adding text before the line 4  ############
+echo Adding text before the line 4 
 
 sed '4 i #This is the extra line' ./input.txt
-echo #####adding text after the line 4   ############
+echo adding text after the line 4 
 sed '5 a #This is the extra line' ./input.txt
 
-echo #####Testing awk commnd with the input file ############
+echo Testing awk commnd with the input file 
 
 awk '($2 == "Reason") {print}' ./input.txt
 
