0d577d9403732e4a2ee55bb69f1cdebe910a2aae
Nishanthi-A01 <79563628+Nishanthi-A01@users.noreply.github.com>
2024-11-30 16:29:51 +0530

    intersection of array program

--- scripts/intersectionofarray.py
+++ scripts/intersectionofarray.py
@@ -1,8 +1,8 @@
 def interSection(arr1,arr2): # finding common elements
 
 # using filter method to find identical values via lambda function
-values = list(filter(lambda x: x in arr1, arr2))
-print ("Intersection of arr1 & arr2 is: ",values)
+    values = list(filter(lambda x: x in arr1, arr2))
+    print ("Intersection of arr1 & arr2 is: ",values)
 
 # Driver program
 if __name__ == "__main__":
