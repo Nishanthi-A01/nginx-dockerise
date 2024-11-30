4368b90512651b6bf7ebdb00947552f53c020449
Nishanthi-A01 <79563628+Nishanthi-A01@users.noreply.github.com>
2024-11-30 10:50:58 +0530

    adding sum of even fibonacci number

--- scripts/sumfibonacci.py
+++ scripts/sumfibonacci.py
@@ -1,32 +1,31 @@
 def evenFibSum(limit) :
-    if (limit < 2) :
-        return 0
+    
+    
  
     # Initialize first two even Fibonacci numbers
     # and their sum
     ef1 = 0
     ef2 = 2
     sm= ef1 + ef2
+    i=2 #initialize the fibonacci number counting 
      
     # calculating sum of even Fibonacci value
-    while (ef2 <= limit) :
+    while (i <= limit ) :
  
         # get next even value of Fibonacci 
         # sequence
         ef3 = 4 * ef2 + ef1
  
-        # If we go beyond limit, we break loop
-        if (ef3 > limit) :
-            break
+        
  
         # Move to next even number and update
         # sum
         ef1 = ef2
         ef2 = ef3
+        i= i+1
         sm = sm + ef2
      
     return sm
  
-
-limit = 100
+limit=100
 print(evenFibSum(limit))
