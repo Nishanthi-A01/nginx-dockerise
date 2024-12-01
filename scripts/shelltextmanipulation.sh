75735964db1f79410937b647e0ee1ed7fd9cc2e2
Nishanthi-A01 <79563628+Nishanthi-A01@users.noreply.github.com>
2024-12-01 19:24:17 +0530

    adding shell scripting

--- scripts/shelltextmanipulation.sh
+++ scripts/shelltextmanipulation.sh
@@ -1,5 +1,5 @@
 #Replacing the namespace/username/password in the secret-template file with env variable 
-
+#!/bin/sh
 sed -e 's/%NAMESPACE%/staging/' \
     -e 's/%DB_USERNAME%/admin/'          \
     -e 's/%DB_PASSWORD%/secret/'     \
