52fdc0c0a8bd1e496fef6f5e1e10fd6959e42dd5
Nishanthi-A01 <79563628+Nishanthi-A01@users.noreply.github.com>
2024-12-03 16:09:53 +0530

    changes to sum of decimal

--- scripts/sumofdecimal.py
+++ scripts/sumofdecimal.py
@@ -1,4 +1,5 @@
 import decimal
+
 def sum_n(x, n):
     s = 0
     for i in range(1,n+1):
@@ -14,27 +15,8 @@ if __name__ == "__main__":
    if decimalnum.isdigit() or e >= 0:          
         print("The number entered is valid!")
         total=sum_n(decimal.Decimal(decimalnum),int(iterations))
-        print(total)
+        print('The result of the sum of decimal value:',total)
    else:
         print("The entered number is invalid")
-
-
-    
-mylist=["100","23.0","45.650","56.00","500.70000"]
-
-for item in mylist:
-    print(item)
-    d = decimal.Decimal(item)
-    e=abs(d.as_tuple().exponent) 
-    if e==0:
-         print(f"{item} has no decimal places")
-    elif e==1:
-            print(f"{item} has 1 decimal place")
-    elif e==2:
-            print(f"{item} has 2 decimal place")
-    elif e==3:
-            print(f"{item} has 3 decimal place")
-    else:
-            print(f"{item} is has many decimal places")
 
 
