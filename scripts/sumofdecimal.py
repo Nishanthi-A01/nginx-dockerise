91b5155e1d204067ac7386650ea6228681b709ca
Nishanthi-A01 <79563628+Nishanthi-A01@users.noreply.github.com>
2024-12-03 12:52:44 +0530

    changes to sum of decimal

--- scripts/sumofdecimal.py
+++ scripts/sumofdecimal.py
@@ -9,6 +9,7 @@ def sum_n(x, n):
 if __name__ == "__main__":
    decimalnum = input("Enter the decimal number:")
    iterations = input("Enter the number of iterations:")
+   print(type(decimalnum))
    if decimalnum.isdecimal():
       print("The number entered is valid!")
       total=sum_n(Decimal(decimalnum),int(iterations))
